97ee591cf7eb3fee8891be521776179676802609
Wabmaister <99593292+Wabmaister@users.noreply.github.com>
2022-02-19 19:58:20 +0100

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,11 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'Scots', // This changes the display name for your language
+  language: '', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'Scots-Online', // Put your name here so people know who made this Wordle!
-  authorWebsite: 'www.scots-online.org', // Put a link to your website or social media here
-  wordListSource: 'Scots Online Dictionary', // Describe the source material for your words here
-  wordListSourceLink: 'www.scots-online.dictionary', // Put a link to the source material for your words here
+  authorWebsite: 'https://www.scots-online.org', // Put a link to your website or social media here
+  wordListSource: 'https://Scots Online Dictionary', // Describe the source material for your words here
+  wordListSourceLink: https://'www.scots-online.dictionary', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
